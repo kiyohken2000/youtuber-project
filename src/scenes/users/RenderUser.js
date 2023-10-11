89470887f487ca3f32d17db7d23955c76c9aad73
kiyohken2000 <www.broadwell.com@gmail.com>
2023-10-12 00:58:45 +0900

    もっと読む機能を追加

--- src/scenes/users/RenderUser.js
+++ src/scenes/users/RenderUser.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { fontSize, colors } from "../../theme";
+import ReadMore from '@fawazahmed/react-native-read-more';
 
 export default function RenderUser(props) {
-  const { id, name, age, city } = props.item
+  const { id, name, age, city, comment } = props.item
 
   return (
     <View>
@@ -11,6 +12,16 @@ export default function RenderUser(props) {
       <Text style={styles.title}>name: {name}</Text>
       <Text style={styles.title}>age: {age}</Text>
       <Text style={styles.title}>city: {city}</Text>
+      <ReadMore
+        numberOfLines={3}
+        style={styles.comment}
+        seeMoreText="もっと読む"
+        seeLessText="閉じる"
+        seeMoreStyle={{color: colors.bluePrimary}}
+        seeLessStyle={{color: colors.purple}}
+      >
+        {comment}
+      </ReadMore>
       <View style={{width: '100%', backgroundColor: colors.gray, paddingVertical: 1}} />
     </View>
   )
@@ -22,5 +33,8 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: fontSize.xxxLarge
+  },
+  comment: {
+    fontSize: fontSize.xLarge
   }
 })
